28dea334d9d0c066a4f3f1891ef9ebd9911d1674
Ppp <you@example.com>
2016-05-20 14:44:19 +0300

    test

--- src/app.js
+++ src/app.js
@@ -162,7 +162,7 @@ function processEvent(event) {
 						}, 2000);
 						
 				};	
-				if (action=='chartyes' )					
+				if (action=='chartyes' && response.result.actionIncomplete==false  )					
 						{		
 						var repl123='http://195.251.59.51/json/photorig/therm_chart2.jpg';
 						//var repl456='http://195.251.59.51/json/photorig/therm_chart.jpg';
@@ -290,7 +290,7 @@ function processEvent(event) {
 						}, 2000);
 						
 				};	
-				if (action=='chartyes' )					
+				if (action=='chartyes' && response.result.actionIncomplete==false )					
 						{		
 						var repl123='http://195.251.59.51/json/photorig/therm_chart2.jpg';
 						//var repl456='http://195.251.59.51/json/photorig/therm_chart.jpg';
