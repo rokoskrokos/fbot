fefeb1d335e5a8e02c4e711c61ce7f25b43ece6d
Ppp <you@example.com>
2016-06-13 16:18:15 +0300

    test

--- src/app.js
+++ src/app.js
@@ -265,12 +265,12 @@ function processEvent(event) {
 						
 						sendFBMessage(sender, messageData34);
 						
-						var textArray3344 = ['Now for the leafs :-) ','Leafs analysis on the way', 'We continue with leafs?','Next leafs analysis','Leafs next','Leafs analysis next'];
+						var textArray3344 = ['Now for the leafs :-) ','Leafs analysis on the way:', 'We continue with leafs?','Next leafs analysis:','Leafs next:','Leafs analysis next'];
 						var randomNumber3344 = Math.floor(Math.random()*textArray3344.length);			
 						setTimeout(function(){
 						var wrwr3344=textArray3344[randomNumber3344];					
 						sendFBMessage(sender, {text: wrwr3344});
-						}, 2000);
+						}, 1500);
 						
 						setTimeout(function(){
 						repl34='http://195.251.59.51/json/photorig/leafs.jpg';
@@ -293,7 +293,7 @@ function processEvent(event) {
 						  var ttt=anythingelse();
 						  setTimeout(function(){						
 						  sendFBMessage(sender, {text: ttt});
-						  }, 2000);
+						  }, 2500);
 						
 						//var textArray1 = ['How about a graph? :-) ','Do you also want a graph?', 'Do you also want a chart?','How about a chart?','Need a graph?','Need chart?'];
 						//var randomNumber1 = Math.floor(Math.random()*textArray1.length);			
