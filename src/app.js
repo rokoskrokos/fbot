d20214b1701e705aa2285a658c12e0b2e213934f
Ppp <you@example.com>
2016-06-13 16:43:31 +0300

    test

--- src/app.js
+++ src/app.js
@@ -268,12 +268,12 @@ function processEvent(event) {
 						
 						var textArray3344 = ['Now for the leafs :-) ','Leafs analysis on the way:', 'We continue with leafs:','Next leafs analysis:','Leafs next:','Leafs analysis next'];
 						var randomNumber3344 = Math.floor(Math.random()*textArray3344.length);		
-						//setTimeout(function(){
+						setTimeout(function(){
 						var wrwr3344=textArray3344[randomNumber3344];					
 						sendFBMessage(sender, {text: wrwr3344});
-						//}, 4000);
+						}, 2000);
 						
-						//setTimeout(function(){
+						setTimeout(function(){
 						repl34='http://195.251.59.51/json/photorig/leafs.jpg';
 						var messageData34 ={
 											"attachment":{
@@ -286,7 +286,7 @@ function processEvent(event) {
 										  
 						
 						sendFBMessage(sender, messageData34);
-						//}, 1000);
+						}, 2000);
 						
 						
 											
